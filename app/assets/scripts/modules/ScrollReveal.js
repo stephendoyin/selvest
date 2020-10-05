8cfa62053680585368c9cb78c3439f08c8511b26
stephendoyin <stevedoyin@gmail.com>
2020-10-05 13:45:21 +0100

    added scroll animation

--- app/assets/scripts/modules/ScrollReveal.js
+++ app/assets/scripts/modules/ScrollReveal.js
@@ -1,5 +1,9 @@
 class ScrollRevealScript {
   constructor() {
+    this.startScrollReveal();
+  }
+
+  startScrollReveal() {
     ScrollReveal().reveal(".steps__item", {
       scale: 1,
       duration: 1500,
@@ -8,7 +12,38 @@ class ScrollRevealScript {
       origin: "bottom",
       distance: "50px",
     });
-    // this.startScrollReveal();
+    ScrollReveal().reveal(".js-slide-up", {
+      scale: 1,
+      duration: 1500,
+      interval: 30,
+      mobile: false,
+      origin: "bottom",
+      distance: "50px",
+    });
+    ScrollReveal().reveal(".js-slide-left-in", {
+      scale: 1,
+      duration: 1500,
+      interval: 30,
+      mobile: false,
+      origin: "left",
+      distance: "50px",
+    });
+    ScrollReveal().reveal(".js-slide-right-in", {
+      scale: 1,
+      duration: 1500,
+      interval: 30,
+      mobile: false,
+      origin: "right",
+      distance: "50px",
+    });
+    ScrollReveal().reveal(".js-scale-up", {
+      delay: 0,
+      distance: "200px",
+      duration: 1000,
+      easing: "ease-in",
+      scale: 0.2,
+      opacity: 0.05,
+    });
   }
 }
 
